35d8feef3e023fdea1f1b08dafa118dd2aeaae31
aelnona <ubiquitous.kaleidoscope.9@gmail.com>
2022-09-01 12:31:13 +0100

    SICP 1.1-1.7

---
+++ post/sicp/1.3.scm
@@ -0,0 +1,27 @@
+;; SICP ex 1.3
+
+(define (f x y z)
+  (define (square a) (* a a))
+  (define (sumsquare a b) (+ (square a) (square b)))
+  (cond ((and (< x y) (< x z))
+	 (sumsquare y z))
+	((and (< y x) (< y z))
+	 (sumsquare x z))
+	((and (< z x) (< z y))
+	 (sumsquare x y))
+	;; Find largest
+	(else (cond ((= x y z)
+		     (sumsquare x y))
+		    ((= x y)
+		     (sumsquare x z))
+		    ((= x z)
+		     (sumsquare x y))
+		    ((= y z)
+		     (sumsquare x z))))))
+
+(list (f 1 2 3)
+      (f 3 2 1)
+      (f 3 3 1)
+      (f 1 1 3)
+      (f 5 5 5)
+      (f 5 1 5))
